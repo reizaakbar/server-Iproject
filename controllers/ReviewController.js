3c6d9e00b5f28ac5a3adf7879db9aa74ade3d90c
reizaakbar <reiwildani@gmail.com>
2024-09-06 14:49:57 +0700

    revisi review controller

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -8,9 +8,9 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage }).single("image");
 
 cloudinary.config({
-  cloud_name: "dcisb7ayn",
-  api_key: "258396789225291",
-  api_secret: "GjyyD6fm9tLauJNOcKz0CN-yVmY",
+  cloud_name: "dg7mic8zj",
+  api_key: "716194785367285",
+  api_secret: "8Ybk3sfnaf_LnOBKgtacIWs5aXU",
 });
 
 class ReviewController {
